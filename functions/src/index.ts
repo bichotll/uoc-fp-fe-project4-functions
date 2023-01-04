7886f2608f3bc221a5e7fdedd148f1e1e44c4042
Jaume Tarradas Llort <jaume@bic.cat>
2023-01-04 18:52:22 +0100

    all done

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,20 +4,11 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const sendNotification = async (msg: string) => {
 
     functions.logger.log(msg);
 
     // Notification details.
-    // const text = snapshot.data().text;
     const payload = {
         notification: {
             title: msg,
@@ -27,23 +18,20 @@ const sendNotification = async (msg: string) => {
         }
     };
 
-    return await admin.messaging().sendToDevice([
-        'epWHn-74ZELV9qJdKKUayA:APA91bGTT3UxdNXPnhdN8Sf-dJmISBKUtbKuuto_vKzW7glqKjlM9J-E0vYH7EfygfvJE3AvWB4HBvJo7QmK6rNOdeA4o7nT2cVFAUUn4VigmLYCHzta6Lhr3XGUKWWkVBncSEVJvUCS'
-    ], payload);
+    // Get the list of device tokens.
+    const allTokens = await admin.firestore().collection('tokens').get();
+    const tokens: any = [];
+    allTokens.forEach((tokenDoc) => {
+        tokens.push(tokenDoc.data().tokenId);
+    });
+    functions.logger.log('tokens length: ', tokens.length);
 
-    // // Get the list of device tokens.
-    // const allTokens = await admin.firestore().collection('fcmTokens').get();
-    // const tokens = [];
-    // allTokens.forEach((tokenDoc) => {
-    //     tokens.push(tokenDoc.id);
-    // });
-
-    // if (tokens.length > 0) {
-    //     // Send notifications to all tokens.
-    //     const response = await admin.messaging().sendToDevice(tokens, payload);
-    //     await cleanupTokens(response, tokens);
-    //     functions.logger.log('Notifications have been sent and tokens cleaned up.');
-    // }
+    if (tokens.length > 0) {
+        // Send notifications to all tokens.
+        await admin.messaging().sendToDevice(tokens, payload);
+        // await cleanupTokens(response, tokens);
+        functions.logger.log('Notifications have been sent and tokens cleaned up.');
+    }
 }
 
 exports.sendNotificationsOnDocumentCreate = functions.firestore.document('Jugadores/{messageId}').onCreate(
